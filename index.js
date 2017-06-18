9fa0bda7d58c8ee0375f1bbc9bdc674f8c6ab018
Iahoffmann <hoffmannisaac@gmail.com>
2017-06-18 12:01:18 -0500

    Added in linting, and func for checking for straights and flushes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const readline = require('readline');
 const evaluator = require('./lib/util.js');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'Please enter your hand> '
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Please enter your hand> '
 });
 
 rl.prompt();
 
 rl.on('line', (line) => {
-  const testString = 'Kh Qh 6h 2h 9h'; //TODO REPLACE
-  evaluator.evaluateHand(testString);
-  rl.prompt();
+    const testString = '6s 2h 3d 4c 5s'; //TODO REPLACE
+    console.log(evaluator.evaluateHand(testString));
+    rl.prompt();
 }).on('close', () => {
-  console.log('Have a great day!');
-  process.exit(0);
+    console.log('Have a great day!');
+    process.exit(0);
 });
